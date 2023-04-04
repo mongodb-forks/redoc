e0a6edfebefdde79290a6684858421a4e41be12c
branberry <brandon.ly@mongodb.com>
2023-04-04 12:13:49 -0500

    [DOP-3497]: Comment code and use numbers

--- src/components/VersionSelector/VersionSelector.tsx
+++ src/components/VersionSelector/VersionSelector.tsx
@@ -24,7 +24,7 @@ const VersionSelectorComponent = ({
 }: VersionSelectorProps): JSX.Element => {
   const initialSelectedIdx = resourceVersions.indexOf(active.resourceVersion);
   const [open, setOpen] = React.useState<boolean>(false);
-  const [focusedIdx, setFocusedIdx] = React.useState<number | null>(null);
+  const [focusedIdx, setFocusedIdx] = React.useState<number>(-1);
   const [selectedIdx, setSelectedIdx] = React.useState<number>(initialSelectedIdx);
 
   const menuListRef = React.useRef(null);
@@ -34,7 +34,7 @@ const VersionSelectorComponent = ({
       <Option
         key={`option-${i}`}
         selected={i === selectedIdx}
-        focused={i === focusedIdx && focusedIdx !== selectedIdx}
+        focused={i === focusedIdx}
         option={option}
         onClick={() => handleClick(i)}
       />
@@ -42,7 +42,9 @@ const VersionSelectorComponent = ({
   });
 
   useOutsideClick(menuListRef, () => {
+    console.log('outside click');
     if (open) setOpen(false);
+    setFocusedIdx(0);
   });
 
   const handleClick = (idx: number) => {
@@ -51,12 +53,42 @@ const VersionSelectorComponent = ({
   };
 
   const handleFocusChange = (event: React.KeyboardEvent<HTMLDivElement>) => {
-    console.log(event.key);
+    const { key, shiftKey } = event;
+
+    if (key === 'ArrowDown' || (key === 'Tab' && !shiftKey)) {
+      // if we go down when we are already past the end, don't do anything
+      if (focusedIdx === resourceVersions.length) return;
+
+      if (focusedIdx === -1) {
+        // when first entering the dropdown via the down arrow key or tab,
+        // we want to open the modal
+        setOpen(true);
+      } else if (focusedIdx === resourceVersions.length - 1) {
+        // if we are at the last element of the dropdown, and attempt to go
+        // down again, we want to close the dropdown
+        setOpen(false);
+      }
+      setFocusedIdx(focusedIdx + 1);
+    } else if (key === 'ArrowUp' || (key === 'Tab' && shiftKey)) {
+      // if we go down when we are already past the end, don't do anything
+      if (focusedIdx === -1) return;
+
+      if (focusedIdx === resourceVersions.length) {
+        // in this scenario, we are entering the dropdown from below
+        // we open the dropdown and start from the bottom
+        setOpen(true);
+      } else if (focusedIdx === 0) {
+        // if we reach the first element in the drop down, and we attempt to go up again,
+        // we want to close the dropdown
+        setOpen(false);
+      }
+
+      setFocusedIdx(focusedIdx - 1);
+    }
   };
-  // const handleArrowKeys = () => {};
 
   return (
-    <StyledWrapper ref={menuListRef} onKeyDown={event => console.log(event.currentTarget)}>
+    <StyledWrapper onKeyDown={handleFocusChange} ref={menuListRef}>
       <StyledSelectWrapper>
         <StyledLabel>Version Selector: v{active.apiVersion}</StyledLabel>
         {description && <StyledDescription>{description}</StyledDescription>}
@@ -70,7 +102,7 @@ const VersionSelectorComponent = ({
         </StyledButton>
       </StyledSelectWrapper>
 
-      <StyledDropdown open={open} onKeyDown={handleFocusChange}>
+      <StyledDropdown open={open}>
         <div>
           <StyledMenuList>{options}</StyledMenuList>
         </div>
