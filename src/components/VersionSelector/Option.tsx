875d3201d30e52e997b19e687ad051c682069806
Matt Meigs <matt.meigs@mongodb.com>
2023-04-05 16:17:47 -0400

    completed working version

--- src/components/VersionSelector/Option.tsx
+++ src/components/VersionSelector/Option.tsx
@@ -4,24 +4,8 @@ import Checkmark from './CheckmarkSvg';
 import { OptionProps } from './types';
 
 export const Option = ({ option, value, selected, onClick, focused }: OptionProps) => {
-  const KEY_ENTER = 'Enter';
-  const KEY_SPACE = ' ';
-
-  const handleKeyPress = (event: React.KeyboardEvent) => {
-    console.log('key press ', event.key);
-    if (event.key === KEY_ENTER || event.key === KEY_SPACE) {
-      console.log('we are keying', value);
-      onClick(value);
-    }
-  };
-
   return (
-    <StyledLi
-      onClick={() => onClick(value)}
-      onKeyUp={handleKeyPress}
-      selected={selected}
-      focused={focused}
-    >
+    <StyledLi onClick={() => onClick(value)} selected={selected} focused={focused}>
       {selected ? <Checkmark /> : <StyledPlaceholder />}
       <StyledOptionText>{option}</StyledOptionText>
     </StyledLi>
--- src/components/VersionSelector/VersionSelector.tsx
+++ src/components/VersionSelector/VersionSelector.tsx
@@ -24,15 +24,75 @@ const VersionSelectorComponent = ({
   description,
   rootUrl,
 }: VersionSelectorProps): JSX.Element => {
-  const reversedResourceVersions = resourceVersions.slice().reverse();
-  const initialSelectedIdx = reversedResourceVersions.indexOf(active.resourceVersion);
+  const descendingResourceVersions = resourceVersions.slice().reverse();
+  const initialSelectedIdx = descendingResourceVersions.indexOf(active.resourceVersion);
   const [open, setOpen] = React.useState<boolean>(false);
   const [focusedIdx, setFocusedIdx] = React.useState<number>(initialSelectedIdx);
   const [selectedIdx, setSelectedIdx] = React.useState<number>(initialSelectedIdx);
 
   const menuListRef = React.useRef(null);
+  useOutsideClick(menuListRef, () => {
+    if (open) setOpen(false);
+  });
 
-  const options = reversedResourceVersions.map((option, i) => {
+  const handleKeyDownSelect = React.useCallback(() => {
+    if (focusedIdx === selectedIdx) return setOpen(false);
+    const resourceVersionReal = descendingResourceVersions[focusedIdx];
+
+    // navigate to resource version spec
+    let selectedResourceVersionUrl = `${rootUrl}/${resourceVersionReal}`;
+    const anchorTagIdx = window.location.href.indexOf('#tag');
+    if (anchorTagIdx > -1) {
+      selectedResourceVersionUrl += window.location.href.slice(anchorTagIdx);
+    }
+    console.log(selectedResourceVersionUrl);
+    // window.location.href = selectedResourceVersionUrl;
+    setSelectedIdx(focusedIdx);
+    return setOpen(false);
+  }, [selectedIdx, rootUrl, focusedIdx, descendingResourceVersions]);
+
+  const handleClick = (idx: number, resourceVersion: string) => {
+    if (idx === selectedIdx) return setOpen(false);
+
+    // navigate to resource version spec
+    let selectedResourceVersionUrl = `${rootUrl}/${resourceVersion}`;
+    const anchorTagIdx = window.location.href.indexOf('#tag');
+    if (anchorTagIdx > -1) {
+      selectedResourceVersionUrl += window.location.href.slice(anchorTagIdx);
+    }
+    console.log(selectedResourceVersionUrl);
+    // window.location.href = selectedResourceVersionUrl;
+    setFocusedIdx(idx);
+    setSelectedIdx(idx);
+    return setOpen(false);
+  };
+
+  const handleFocusChange = React.useCallback(
+    (event: React.KeyboardEvent<HTMLDivElement>) => {
+      const { key, shiftKey } = event;
+      if (key === 'Enter' || key === ' ') {
+        handleKeyDownSelect();
+      } else if (key === 'ArrowDown' || (key === 'Tab' && !shiftKey)) {
+        // if we go down when we are already past the end, don't do anything
+        if (focusedIdx === descendingResourceVersions.length - 1) setOpen(false);
+        else if (focusedIdx === -1) {
+          // when first entering the dropdown via the down arrow key or tab,
+          // we want to open the modal
+          setOpen(true);
+        }
+
+        setFocusedIdx(focusedIdx + 1);
+      } else if (key === 'ArrowUp' || (key === 'Tab' && shiftKey)) {
+        // if at top, close VersionSelector
+        if (focusedIdx === 0) return setOpen(false);
+
+        setFocusedIdx(focusedIdx - 1);
+      }
+    },
+    [focusedIdx, descendingResourceVersions, handleKeyDownSelect],
+  );
+
+  const options = descendingResourceVersions.map((option, i) => {
     return (
       <Option
         key={`option-${i}`}
@@ -45,73 +105,6 @@ const VersionSelectorComponent = ({
     );
   });
 
-  useOutsideClick(menuListRef, () => {
-    if (open) setOpen(false);
-    setFocusedIdx(0);
-  });
-
-  const handleClick = React.useCallback(
-    (idx: number, resourceVersion: string) => {
-      console.log('handleClick ', idx, resourceVersion);
-      if (idx === selectedIdx) return setOpen(false);
-
-      // navigate to resource version spec
-      let selectedResourceVersionUrl = `${rootUrl}/${resourceVersion}`;
-      const anchorTagIdx = window.location.href.indexOf('#tag');
-      if (anchorTagIdx > -1) {
-        selectedResourceVersionUrl += window.location.href.slice(anchorTagIdx);
-      }
-      console.log(selectedResourceVersionUrl);
-      // window.location.href = selectedResourceVersionUrl;
-      setFocusedIdx(idx);
-      setSelectedIdx(idx);
-      return setOpen(false);
-    },
-    [selectedIdx, rootUrl],
-  );
-
-  const handleFocusChange = React.useCallback(
-    (event: React.KeyboardEvent<HTMLDivElement>) => {
-      const { key, shiftKey } = event;
-      console.log(key, shiftKey);
-      console.log('focusedIdx before ', focusedIdx);
-
-      if (key === 'ArrowDown' || (key === 'Tab' && !shiftKey)) {
-        // if we go down when we are already past the end, don't do anything
-        if (focusedIdx === reversedResourceVersions.length - 1) return;
-
-        if (focusedIdx === -1) {
-          // when first entering the dropdown via the down arrow key or tab,
-          // we want to open the modal
-          setOpen(true);
-        } else if (focusedIdx === reversedResourceVersions.length - 1) {
-          // if we are at the last element of the dropdown, and attempt to go
-          // down again, we want to close the dropdown
-          return setOpen(false);
-        }
-        console.log('after ', reversedResourceVersions[focusedIdx + 1]);
-        setFocusedIdx(focusedIdx + 1);
-      } else if (key === 'ArrowUp' || (key === 'Tab' && shiftKey)) {
-        // if we go down when we are already past the end, don't do anything
-        if (focusedIdx === 0) return setOpen(false);
-
-        if (focusedIdx === reversedResourceVersions.length) {
-          // in this scenario, we are entering the dropdown from below
-          // we open the dropdown and start from the bottom
-          setOpen(true);
-        }
-        // else if (focusedIdx === 0) {
-        //   // if we reach the first element in the drop down, and we attempt to go up again,
-        //   // we want to close the dropdown
-        //   setOpen(false);
-        // }
-
-        setFocusedIdx(focusedIdx - 1);
-      }
-    },
-    [focusedIdx, reversedResourceVersions],
-  );
-
   return (
     <StyledWrapper onKeyDown={handleFocusChange} ref={menuListRef}>
       <StyledSelectWrapper>
@@ -119,7 +112,7 @@ const VersionSelectorComponent = ({
         {description && <StyledDescription>{description}</StyledDescription>}
         <StyledButton onClick={() => setOpen(!open)}>
           <StyledDisplay>
-            <StyledSelected>{reversedResourceVersions[selectedIdx]}</StyledSelected>
+            <StyledSelected>{descendingResourceVersions[selectedIdx]}</StyledSelected>
             <ArrowIcon open={open} />
           </StyledDisplay>
         </StyledButton>
